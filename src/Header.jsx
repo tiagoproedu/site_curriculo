904eb1d3eaa9c573dc2b4686651d58623fbb5dcb
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-07-01 18:41:14 -0300

    atualizando header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,17 @@ function Header() {
         <>
             <div className="header">
                 <div className="perfil">
-                    <img src="https://avatars.githubusercontent.com/u/71508709?s=48&v=4" alt="foto_perfil" />
                     <h1>Tiago Silva</h1>
                 </div>
                 <ul className="navbar">
                     <li><a href="/">Home</a></li>
+                    <li><a>About</a></li>
                     <li><a href="/repos">Portfolio</a></li>
-                    <li>Contatos</li>
+                    <li>Blog</li>
                 </ul>
+                <div className="talk">
+                    <button>Vamos conversar!</button>
+                </div>
             </div>
         </>
     );
